18f5209006360a1e196fbe72b7d1b9fb81a6d682
eunxoo <dmstn7989@naver.com>
2023-08-28 21:39:23 +0900

    test

--- server/TodayWeather.js
+++ server/TodayWeather.js
@@ -43,7 +43,8 @@ module.exports = async (req, res) => {
   const toXYconvert = toXY(lat, lon);
 
   const url = `http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst`;
-  const SERVICE_KEY = process.env.OPENAPI_KEY;
+  const SERVICE_KEY =
+    "RSDUo%2F0cItbh7%2BcVxTEXEsk7LwF5pQx%2FOV%2F01Jvv3Y1F4%2FUNvQAR%2BcSzl29QAKD3l%2Bv3KR7f86b5MPQ%2FCE834A%3D%3D";
   console.log(req.body);
   const apiUrl =
     url +
