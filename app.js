4091ee8a846126dde3de45b457f6057fa3819e06
eunxoo <dmstn7989@naver.com>
2023-08-28 21:43:36 +0900

    test

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const NowWeather = require("./server/NowWeather");
-const TodayWeather = require("./server/TodayWeather");
-const YesWeather = require("./server/YesWeather");
-const AddressConvert = require("./server/AddressConvert");
-const DustWeather = require("./server/DustWeather");
-const TomorrowWeather = require("./server/TomorrowWeather");
-const PORT = 3000;
+// const express = require("express");
+// const app = express();
+// const cors = require("cors");
+// const NowWeather = require("./server/NowWeather");
+// const TodayWeather = require("./server/TodayWeather");
+// const YesWeather = require("./server/YesWeather");
+// const AddressConvert = require("./server/AddressConvert");
+// const DustWeather = require("./server/DustWeather");
+// const TomorrowWeather = require("./server/TomorrowWeather");
+// const PORT = 3000;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  })
-);
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//     credentials: true,
+//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//   })
+// );
 
-app.post("/nowweather", NowWeather);
-app.post("/todayweather", TodayWeather);
-app.post("/api/yesweather", YesWeather);
-app.post("/api/tomorrowweather", TomorrowWeather);
-app.get("/api/convert", AddressConvert);
-app.post("/api/nowdust", DustWeather);
+// app.post("/nowweather", NowWeather);
+// app.post("/todayweather", TodayWeather);
+// app.post("/api/yesweather", YesWeather);
+// app.post("/api/tomorrowweather", TomorrowWeather);
+// app.get("/api/convert", AddressConvert);
+// app.post("/api/nowdust", DustWeather);
 
-app.listen(PORT, () => {
-  console.log(`server running on PORT ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`server running on PORT ${PORT}`);
+// });
